a46f55d3ce674950959cb65a259bdf0952e57f93
Irvin <irvin.avalos213@gmail.com>
2025-11-09 13:10:40 -0800

    added functionalty for shfiting bits in chessboard

--- board.cpp
+++ board.cpp
@@ -1,13 +1,14 @@
 #include "board.hpp"
 
 int printBoard(u64 board) {
-    if (!board) { return 1; }
+    std::printf("-------------------------\n");
 
     u64 shift = 1ull;
     std::size_t r;
     std::size_t c;
 
     for (r = numRows; r > 0; --r) {
+        std::printf("%zu  ", r);
         for (c = 0; c < numCols; ++c) {
             std::size_t square = getSquare(r - 1, c);
 
@@ -19,6 +20,12 @@ int printBoard(u64 board) {
         }
         std::printf("\n");
     }
+    std::printf("\n    a b c d e f g h\n");
 
+    return 0;
+}
+
+int shiftBoard(u64 *board, square sq) {
+    *board |= (1ull << (u64)sq);
     return 0;
 }
--- board.hpp
+++ board.hpp
@@ -8,3 +8,5 @@ inline const std::size_t getSquare(std::size_t row, std::size_t col) {
 }
 
 int printBoard(u64 board);
+
+int shiftBoard(u64 *board, square sq);

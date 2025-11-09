d65a47ca2c2662a0450328f0d8897016090a65c7
Irvin <irvin.avalos213@gmail.com>
2025-11-09 13:10:55 -0800

    testing shift board function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "board.hpp"
 
 int main(void) {
-    u64 board = 3ull;
-    int res = printBoard(board);
+    int res;
+    u64 board = 0ull;
+    res = shiftBoard(&board, square::a1);
+    res = shiftBoard(&board, square::a3);
+    res = shiftBoard(&board, square::c1);
+    res = shiftBoard(&board, square::c3);
+    res = printBoard(board);
     return res;
 }

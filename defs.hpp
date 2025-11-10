0bdfb9d831255aa9ff441b6875374d7bf61b6a24
Irvin <irvin.avalos213@gmail.com>
2025-11-09 16:35:48 -0800

    update(defs): enum class -> enum : u64, no need to cast to u64 anymore

--- defs.hpp
+++ defs.hpp
@@ -5,7 +5,7 @@
 typedef unsigned long long u64;
 
 // clang-format off
-enum class square {
+enum square : u64 {
     a1, b1, c1, d1, e1, f1, g1, h1,
     a2, b2, c2, d2, e2, f2, g2, h2,
     a3, b3, c3, d3, e3, f3, g3, h3,

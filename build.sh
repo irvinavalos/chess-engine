754fbcbe8058d7c214290810786097c063b1a570
Irvin <irvin.avalos213@gmail.com>
2025-11-13 03:02:42 -0800

    update: build script now correctly builds .cpp files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-clang++ -std=c++20 -Wall -Wextra -Wpedantic board.cpp main.cpp -o main && ./main
+clang++ -std=c++20 -Wall -Wextra -Wpedantic -Iinclude src/board.cpp src/main.cpp -o main && ./main
